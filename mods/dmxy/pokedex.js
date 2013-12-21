4314b530726201b68db233905e3b435bf6865a8f
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-12-20 21:15:04 -0800

    save point 1

--- mods/dmxy/pokedex.js
+++ mods/dmxy/pokedex.js
@@ -1,1 +1,3 @@
-lelz
+exports.BattlePokedex = {
+meganium:{num:154,species:"Meganium",types:["Grass"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:80,atk:82,def:100,spa:83,spd:100,spe:80},abilities:{0:"Overgrow",H:"Leaf Guard"},heightm:1.8,weightkg:100.5,color:"Green",prevo:"bayleef",evoLevel:32,eggGroups:["Monster","Grass"]
+mewtwomegax:{num:154,species:"Mewtwo-Mega-X",baseSpecies:"Mewtwo",forme:"Mega-X",formeLetter:"X",types:["Psychic","Fighting"],baseStats:{hp:106,atk:190,def:100,spa:154,spd:100,spe:130},abilities:{0:"Steadfast"},heightm:2.3,weightkg:127,color:"Purple",eggGroups:["Undiscovered"]},
