e1ebb83edc9166af5904cdc05927c3d31bcc119e
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-12-21 09:02:32 -0800

    try to fix power herb

--- mods/dmxy/items.js
+++ mods/dmxy/items.js
@@ -1,2 +1,25 @@
 exports.BattleItems = {
+        "powerherb": {
+                id: "powerherb",
+                onChargeMove: function(pokemon, target, move) {
+                        if (pokemon.useItem()) {
+                                this.debug('power herb - remove charge turn for '+move.id);
+                                return false; // skip charge turn
+                        }
+                },
+                onmustrecharge: function(pokemon, target, move) {
+                        if (pokemon.useItem()) {
+                                pokemon.removeVolatile('mustrecharge');
+                                return true; // skip charge turn
+                        }
+                },
+                name: "Power Herb",
+                spritenum: 358,
+                fling: {
+                        basePower: 10
+                },
+                num: 271,
+                gen: 4,
+                desc: "Holder's two-turn moves complete in one turn (except Sky Drop). Single use."
+        },
 };
