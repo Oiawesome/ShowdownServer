fe4f0d4c4ff97254b8b661debe47d6a0ea5fb6de
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-12-21 07:42:57 -0800

    finish learnsets

--- mods/dmxy/scripts.js
+++ mods/dmxy/scripts.js
@@ -1,1 +1,23 @@
-sasd
+exports.BattleScripts = {
+        init: function() { 
+          this.modData('Learnsets', 'raticate').learnset.poisonjab = ['6M'];
+          
+          this.modData('Learnsets', 'aerodactyl').learnset.bravebird = ['6L50'];
+          
+          this.modData('Learnsets', 'mewtwo').learnset.shadowclaw = ['6L40'];
+          this.modData('Learnsets', 'mewtwo').learnset.hijumpkick = ['6L75'];
+          this.modData('Learnsets', 'mewtwo').learnset.swordsdance = ['6M'];
+          
+          this.modData('Learnsets', 'ampharos').learnset.dracometeor = ['6T'];
+          this.modData('Learnsets', 'ampharos').learnset.tailglow = ['6L75'];
+          
+          this.modData('Learnsets', 'wobbuffet').learnset.recover = ['6L100'];
+          this.modData('Learnsets', 'wobbuffet').learnset.taunt = ['6M'];
+          
+          this.modData('Learnsets', 'aggron').learnset.slackoff = ['6L88'];
+          
+          this.modData('Learnsets', 'absol').learnset.moonblast = ['6L65'];
+          this.modData('Learnsets', 'absol').learnset.moonlight = ['6L35'];
+          this.modData('Learnsets', 'absol').learnset.dazzlinggleam = ['6M'];
+        }
+};
